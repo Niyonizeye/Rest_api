721dbb799005ef38f1349ccc8f2e59f9603aaa72
unknown <niyoeanpaul@gmail.com>
2021-05-26 09:08:30 +0200

    Authorization

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", " POST, GET,PUT,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
