1c3dd41b06e2740598e3eae376ce9e63c2d902c0
unknown <niyoeanpaul@gmail.com>
2021-05-26 09:42:14 +0200

    hello

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -13,6 +13,7 @@ module.exports = (req, res, next) => {
     decodedToken = jwt.verify(token, 'somesupersecretsecret');
   } catch (err) {
     err.statusCode = 500;
+    console.log(err);
     throw err;
   }
   if (!decodedToken) {
